448da90c5ca5d235027182540ae5328d5925cd2b
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2025-09-18 16:21:49 +0200

    flashlight

--- Assets/Scrips/Flashlight.cs
+++ Assets/Scrips/Flashlight.cs
@@ -1,20 +1,23 @@
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class Flashlight : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public Light lightSource;
-    public Vector2 lightSize;
     void Start()
     {
-        lightSource.areaSize = lightSize;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //toggles flashlight on or off when e is pressed. 
+        if (Input.GetKeyDown("e")){
+            lightSource.enabled = !lightSource.enabled;
+        }
     }
 }
