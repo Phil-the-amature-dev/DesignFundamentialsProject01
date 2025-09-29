b63bc3545ec8f550683e8a9eadc0dfd2463d21ed
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2025-09-29 14:05:30 +0200

    Button Puzzle System (WIP)

---
+++ Assets/Scrips/ButtonPuzzle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ButtonPuzzle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool isCompleted = false;
+    public int buttonGoal;
+    [HideInInspector]
+    public int buttonNum = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scrips/ButtonScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using System;
+
+public class ButtonScript : PlayerActivatable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public ButtonPuzzle puzzle;
+    public int Id;
+    public Material greenState;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override protected void OnActivate()
+    {
+        puzzle.buttonNum += 1;
+        Debug.Log(puzzle.buttonNum);
+        GetComponent<Renderer>().material = greenState;
+        if (puzzle.buttonNum == puzzle.buttonGoal)
+        {
+            //Debug.Log("Goal Reached");
+            puzzle.isCompleted = true;
+        }
+    }
+}
