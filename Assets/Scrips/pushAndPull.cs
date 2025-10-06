8ed4f082464e9804e9ff213dc57ecfae4952421c
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2025-10-06 15:46:18 +0200

    first Iteration of Push and pull system

--- Assets/Scrips/pushAndPull.cs
+++ Assets/Scrips/pushAndPull.cs
@@ -7,29 +7,58 @@ public class pushAndPull : MonoBehaviour
     
     public Camera playerCamera;
     public float maxDistance;
+    public LayerMask layerMask;
+    public Transform headPos;
+    public Transform camRoot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, playerCamera.transform.TransformDirection(Vector3.forward) * 10, Color.red);
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            
+            if (Physics.Raycast(headPos.position, playerCamera.transform.TransformDirection(Vector3.forward) * 10, maxDistance, layerMask))
+            {
+                pull();
+            }     
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (Physics.Raycast(headPos.position, playerCamera.transform.TransformDirection(Vector3.forward) * 10, maxDistance, layerMask))
+            {
+                push();
+            }
         }
     }
 
     void pull()
     {
+        Ray ray = new Ray(headPos.position, playerCamera.transform.TransformDirection(Vector3.forward) * 10);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)){
+            hit.transform.GetComponent<Rigidbody>().AddForce(-camRoot.transform.forward);
+            Debug.Log("Hitt");
+        }
+
+        
+        
         
     }
 
     void push()
     {
-
+        Ray ray = new Ray(headPos.position, playerCamera.transform.TransformDirection(Vector3.forward) * 10);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 pushAngle = Quaternion.AngleAxis(-90, camRoot.right) * camRoot.transform.forward;
+             
+            hit.transform.GetComponent<Rigidbody>().AddForce(pushAngle);
+            Debug.Log("Hitt");
+        }
     }
+    
 }
